a4f5ce232e33254d9069c28e09fa0703bac24657
azamat <azamatkhabibullaevv@gmail.com>
2023-04-30 04:13:36 -0700

    Adding SCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './assets/scss/main.scss'
 import 'remixicon/fonts/remixicon.css'
 
 const root = createRoot(document.getElementById('root'))
