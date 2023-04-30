9f0764fdc71c2b6db67012e85956d1e02ad2bf2c
azamat <azamatkhabibullaevv@gmail.com>
2023-04-30 09:14:12 -0700

    Set primary colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,14 @@
 export const themeSettings = () => {
   return {
+    palette: {
+      primary: {
+        main: 'hsl(79, 72%, 55%)',
+        light: 'hsl(79, 97%, 77%)',
+        dark: 'hsl(79, 63%, 50%)',
+        alt: 'hsl(79, 67%, 52%)',
+        grey: 'hsl(79, 6%, 64%)'
+      }
+    },
     typography: {
       fontFamily: ['Red Hat Display', 'sans-serif'].join(','),
       subtitle1: {
