3fdcd72b2c3dc7a30f9ff14705889de6fafe30e1
azamat <azamatkhabibullaevv@gmail.com>
2023-04-30 05:42:10 -0700

    Basic setup MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+
+const theme = createTheme({})
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>App</div>
+    </ThemeProvider>
+  )
 }
 
 export default App
