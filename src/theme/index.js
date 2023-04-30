345e19a6edb1ffc09b977cde87eb8ce94d8687e8
azamat <azamatkhabibullaevv@gmail.com>
2023-04-30 08:31:32 -0700

    Set subtitle font family

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,6 +2,9 @@ export const themeSettings = () => {
   return {
     typography: {
       fontFamily: ['Red Hat Display', 'sans-serif'].join(','),
+      subtitle1: {
+        fontFamily: ['Kaushan Script', 'cursive'].join(',')
+      }
     }
   }
 }
